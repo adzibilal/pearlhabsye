eb31819e50c81a3e24dadc0d08105479106feddd
adzibilal <adzibilal02@gmail.com>
2024-03-01 16:48:11 +0700

    feat: hero animation

--- src/app/(frontpage)/_components/hero.tsx
+++ src/app/(frontpage)/_components/hero.tsx
@@ -1,21 +1,29 @@
+'use client'
 import Image from 'next/image'
 import React from 'react'
+import { motion } from "framer-motion"
 
 const Hero = () => {
     return (
-        <div className='relative overflow-hidden'>
+        <div className='relative overflow-hidden bg-white'>
             <div className='gradient-top w-full h-52 bg-gradient-to-b from-black/80 to-transparent absolute top-0'></div>
-            <Image
-                src='/images/hero.jpg'
-                width={1920}
-                height={1920}
-                alt=''
-                className='w-screen h-screen object-cover'
-            />
+            <motion.div
+                initial={{ opacity: 0, scale: 1.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 0.5 }}
+                className=''>
+                <Image
+                    src='/images/hero.jpg'
+                    width={1920}
+                    height={1920}
+                    alt=''
+                    className='w-screen h-screen object-cover'
+                />
+            </motion.div>
             <div className='gradient-bottom w-full h-52 bg-gradient-to-t from-black/80 to-transparent absolute bottom-0'></div>
             <div className='max-con z-10 w-full h-full absolute top-0 translate-x-[-50%] left-[50%]'>
                 <div className='absolute bottom-10'>
-                    <div className="mb-3 text-white">
+                    <div className='mb-3 text-white'>
                         Pearl Habsye Blessed Circle <br />
                         Collections 2024
                     </div>
