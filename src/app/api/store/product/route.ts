43083f37196df3309819afd8029d2fd61a323ae4
adzibilal <adzibilal02@gmail.com>
2024-04-20 20:56:17 +0700

    fix(api): fixing api search params

--- src/app/api/store/product/route.ts
+++ src/app/api/store/product/route.ts
@@ -3,25 +3,7 @@ import { NextResponse } from 'next/server'
 
 export async function GET(req: Request) {
     try {
-        const url = new URL(req.url) // Membuat objek URL dari URL permintaan
-        const searchParams = url.searchParams // Mengambil objek URLSearchParams
-
-        const category = searchParams.get('category') // Mendapatkan nilai parameter 'category'
-        const sortBy = searchParams.get('sortBy') // Mendapatkan nilai parameter 'sort'
-
-        let products
-        if (category && sortBy) {
-            products = await db.product.findMany({
-                // where: {
-                //     category: category as string, // asumsikan category adalah string
-                // },
-                orderBy: {
-                    price: sortBy === 'price-low-to-high' ? 'asc' : 'desc'
-                }
-            })
-        } else {
-            products = await db.product.findMany()
-        }
+        const products = await db.product.findMany()
 
         return NextResponse.json(products)
     } catch (error) {
